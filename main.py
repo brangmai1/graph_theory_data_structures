fc6c2de89c2f0cc3c803251344ceda436eada410
Brang Mai <brangmai100@gmail.com>
2022-04-03 14:01:01 -0400

    Implement adjacency list and matrix

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Brang Mai
+from pprint import pprint
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def adjacency_list(*args):
+    adj_list = dict()
+    for edge_pair in args:
+        if edge_pair[1] == '->':
+            right_direction(adj_list, edge_pair)
+        elif edge_pair[1] == '<-':
+            left_direction(adj_list, edge_pair)
+        else:
+            right_direction(adj_list, edge_pair)
+            left_direction(adj_list, edge_pair)
+    return adj_list
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def right_direction(adj_list, pair):
+    node = pair[0]
+    if node in adj_list:
+        adj_list[node].append(pair[2])
+    else:
+        adj_list[node] = [pair[2]]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def left_direction(adj_list, pair):
+    node = pair[2]
+    if node in adj_list:
+        adj_list[node].append(pair[0])
+    else:
+        adj_list[node] = [pair[0]]
+
+
+def nodes_total(*args):
+    set_of_nodes = set()
+    for edge_pair in args:
+        set_of_nodes.add(edge_pair[2])
+    return len(set_of_nodes)
+
+
+def adjacency_matrix(*args):
+    nodes_count = nodes_total(*args)
+
+    adj_matrix = []
+    temp_list = []
+
+    for value in range(nodes_count + 1):
+        temp_list.append(0)
+    for value in range(nodes_count + 1):
+        adj_matrix.append(temp_list[:])
+
+    adj_list = adjacency_list(*args)
+    for node in adj_list.keys():
+        for item in adj_list[node]:
+            # print(f'Node is {node}, item is {item}')
+            adj_matrix[node][item] = 1
+    return adj_matrix
+
+
+if __name__ == "__main__":
+    mal = adjacency_list((1, '->', 2), (1, '->', 3), (2, '->', 1), (2, '<-', 4), (3, '->', 1), (4, '<>', 3),
+                         (4, '<>', 5), (2, '<>', 5))
+    pprint(mal)
+    my_matrix = adjacency_matrix((1, '->', 2), (1, '->', 3), (2, '->', 1), (2, '<-', 4), (3, '->', 1), (4, '<>', 3),
+                         (4, '<>', 5), (2, '<>', 5))
+    pprint(my_matrix)
+
